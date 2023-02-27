601e5acdb9267fd8d6f38e9e036462cacfd625b9
devbmiddle <devbmiddle@gmail.com>
2023-02-27 12:56:11 -0600

    Added DistEvals to DistRanking

--- src/jvmMain/kotlin/DistRanking.kt
+++ src/jvmMain/kotlin/DistRanking.kt
@@ -1,6 +1,7 @@
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -20,7 +21,6 @@ import androidx.compose.ui.unit.dp
 @Preview
 fun DistRanking() {
     Column(Modifier
-        .height(400.dp)
         .padding(10.dp),
         Arrangement.spacedBy(5.dp)
     ) {
@@ -28,14 +28,15 @@ fun DistRanking() {
             columns = GridCells.Adaptive(400.dp),
             horizontalArrangement = Arrangement.spacedBy(10.dp),
             verticalArrangement = Arrangement.spacedBy(10.dp)
-            ) {
-                item (span = { GridItemSpan(1) }){
-                    TestWeight("Chi-Squared Test")
-                }
-                item (span = { GridItemSpan(1) }){
-                    TestWeight("K-S Test")
-                }
+        ) {
+            item (span = { GridItemSpan(1) }){
+                TestWeight("Chi-Squared Test")
+            }
+            item (span = { GridItemSpan(1) }){
+                TestWeight("K-S Test")
+            }
         }
+        RankList()
     }
 }
 
@@ -45,7 +46,9 @@ fun DistRanking() {
 @Preview
 fun TestWeight(testName: String) {
     val (checkedState, onStateChange) = remember { mutableStateOf(false) }
-    Card {
+    Card (
+
+    ) {
         Row(
             Modifier
                 .padding(5.dp),
@@ -96,6 +99,101 @@ fun TestWeight(testName: String) {
                     placeholder = { Text("0") },
                     singleLine = true
                 )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun RankList() {
+    val evals = mutableStateListOf<DistEval>()
+    // Example Evals
+    evals.add(DistEval("Normal", "Good", 0.87436f, 0.7322f, 0.8f))
+    evals.add(DistEval("Weibull", "Bad", 0.19f, 0.21f, 0.2f))
+    evals.add(DistEval("Exponential", "Invalid"))
+    evals.add(DistEval("Gamma", "Good", 0.87436f, 0.7322f, 0.8f))
+    evals.add(DistEval("Binomial", "Bad", 0.19f, 0.21f, 0.2f))
+    evals.add(DistEval("Bernoulli", "Invalid"))
+    evals.add(DistEval("Poisson", "Good", 0.87436f, 0.7322f, 0.8f))
+    evals.add(DistEval("Negative Binomial", "Bad", 0.19f, 0.21f, 0.2f))
+
+    LazyColumn (
+        Modifier
+            .padding(5.dp)
+    ) {
+        if (evals.size > 0) {
+            for (eval in evals) {
+                item {
+                    EvalDisplay(eval)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun EvalDisplay(eval: DistEval) {
+    Card {
+        Row(
+            Modifier
+                .padding(5.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(25.dp)
+        ) {
+            Text(
+                eval.distName,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.width(100.dp)
+            )
+            Text(
+                eval.distGoodness,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.width(50.dp)
+            )
+            if (eval.distGoodness != "Invalid") {
+                Column(
+                    Modifier
+                        .width(175.dp)
+                ) {
+                    Row {
+                        Text(
+                            "Chi-Squared: " + eval.chiSquaredScore.toString(),
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+                    Row {
+                        Text(
+                            "KS: " + eval.ksScore.toString(),
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+                    Row {
+                        Text(
+                            "Score: " + eval.totalScore.toString(),
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+                }
+            } else {
+                Column(
+                    Modifier
+                        .width(175.dp)
+                ) {
+                    Text("")
+                    Text(
+                        "Distribution is Invalid",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                    Text("")
+                }
             }
         }
     }
