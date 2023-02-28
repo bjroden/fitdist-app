fad4d93065399fa2090497d36abf7a740199a17b
bjroden <brianjaredroden@gmail.com>
2023-02-28 14:25:17 -0600

    Bump library version New library version exposes KSL api

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ kotlin {
                 implementation("org.jetbrains.compose.components:components-splitpane-desktop:1.3.0")
                 implementation("org.jetbrains.lets-plot:lets-plot-batik:3.0.0")
                 implementation("org.jetbrains.lets-plot:lets-plot-kotlin-jvm:4.2.0")
-                implementation("org.example:fitdist-kotlin:v0.1")
+                implementation("org.example:fitdist-kotlin:v0.1.1")
             }
         }
         val jvmTest by getting
