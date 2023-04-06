a21af617842cc3e97146d56de41e6ab9c1f12735
Piam Chittisane <pchittisane@gmail.com>
2023-04-06 15:45:53 -0500

    removed imports

--- src/jvmMain/kotlin/DistRanking.kt
+++ src/jvmMain/kotlin/DistRanking.kt
@@ -2,9 +2,6 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.GridItemSpan
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.material3.*
