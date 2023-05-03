7b73fa63681fc825303d02756daefd58a8c5ab15
Piam Chittisane <pchittisane@gmail.com>
2023-05-03 15:47:40 -0500

    DistEval.kt and Histogram.kt removed

--- src/jvmMain/kotlin/DistEval.kt
+++
@@ -1,7 +0,0 @@
-class DistEval(distNameIn: String, distGoodnessIn: String, chiSquaredScoreIn: Float = 0f, ksScoreIn: Float = 0f, totalScoreIn: Float = 0f) {
-    val distName = distNameIn
-    val distGoodness = distGoodnessIn
-    val chiSquaredScore = chiSquaredScoreIn
-    val ksScore = ksScoreIn
-    val totalScore = totalScoreIn
-}
